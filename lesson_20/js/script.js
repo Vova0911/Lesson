cf32ca5ba6e271bd93ed233310adcfa913ca7f76
Vova0911 <volodabart21@gmail.com>
2025-04-16 13:58:52 +0300

    Delete lesson_20 directory

--- lesson_20/js/script.js
+++
@@ -1,9 +0,0 @@
-let documentActions = (e) => {
-  const targetElement = e.target;
-
-  if (targetElement.closest(`.menu__button-burger`)) {
-    document.documentElement.classList.toggle(`open-menu`);
-  }
-};
-
-document.addEventListener(`click`, documentActions);
